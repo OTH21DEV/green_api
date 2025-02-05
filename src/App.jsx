4ab68460f481592f5e04121304b049675e542bce
Oxana Theis <otheis@protonmail.com>
2025-02-05 16:15:47 +0000

    change router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Chat from "./pages/Chat";
 import React, { useState } from "react";
@@ -8,14 +8,14 @@ import React, { useState } from "react";
 function App() {
   const [credentials, setCredentials] = useState(null);
   return (
-    // <BrowserRouter>
-    <Router>
+   <BrowserRouter>
+    {/* // <Router> */}
       <Routes>
         <Route path="/" element={<Login setCredentials={setCredentials} />}></Route>
         <Route path="/chat" element={<Chat credentials={credentials} />}></Route>
       </Routes>
-    {/* // </BrowserRouter> */}
-    </Router>
+      </BrowserRouter> 
+    {/* // </Router> */}
   );
 }
 
